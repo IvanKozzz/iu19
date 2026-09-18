a3dbc5157ed1b303a630d01ff32c817842caf498
IvanKozzz <zaharovivan321@gmail.com>
2026-09-18 22:34:32 +0200

    удален pow.py

--- pow.py
+++
@@ -1,9 +0,0 @@
-def pow(base: float, exp: int):
-    result = 1
-    for _ in range(abs(exp)):
-        result *= base
-    if exp < 0: return 1 / result
-    return result
-
-for i in range(-3, 0):
-    print(pow(2, i))
